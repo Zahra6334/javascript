6c0f8a2971f1afffdafffb7fa01665bd657bae74
jiyana <jiyanaseyhnebi@gmail.com>
2025-07-13 23:41:54 +0300

    for  döngüsü

---
+++ for.js
@@ -0,0 +1,13 @@
+//1 den 10 dahil ekrana yazdırma
+for(var i=1; i<=10;i++){
+    console.log(i)
+}
+
+//1 den 100  kadar olan tekli sayilari  ekrana yazdırma
+for(var i=1; i<=100;i=i+2){
+    console.log(i)
+}
+//2den 100  kadar olan çift sayiları ekrana yazdırır
+for(var i=2; i<=100;i=i+2){
+    console.log(i)
+}
