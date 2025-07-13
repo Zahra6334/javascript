1f76097bbfceef96d174f482f0c3ed71f4147fa0
jiyana <jiyanaseyhnebi@gmail.com>
2025-07-13 23:32:46 +0300

    while döngüsü

---
+++ while.js
@@ -0,0 +1,25 @@
+//1 den 10 kadar sayiları ekrana yazdırma
+var sayi=1
+while(sayi<=10){
+    console.log(sayi)
+    sayi=sayi+1
+}
+//1 den 10  tersten yazrsakta sonsuz kadar sayi yazar 
+/*var sayi=1
+while(sayi<=10){//çünkü şartımız burada ve 10 küç olduğu sürece döngü devam eder
+    console.log(sayi)
+    sayi=sayi-1
+}*/
+
+//2 n 100 kadar çift sayiları ekrana yazdırma
+var sayi=2
+while(sayi<=100){
+    console.log(sayi)
+    sayi=sayi+2
+}
+//1 den  100 kadar tek sayıları ekrana yazdırma
+var sayi=1// 1 den başlamali
+while(sayi<=100){
+    console.log(sayi)
+    sayi=sayi+2
+}
