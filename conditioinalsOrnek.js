2e02e0d80f56b4c6142a2389fec64d555ff0af62
jiyana <jiyanaseyhnebi@gmail.com>
2025-07-13 21:56:08 +0300

    şart blokları örneği

---
+++ conditioinalsOrnek.js
@@ -0,0 +1,20 @@
+var sayi1=29
+var sayi2=30
+var sayi3=2
+//en buyuk sayı bulun 
+if(sayi1<sayi2){
+    console.log(sayi2+'en buyuk')
+}else if(sayi1>sayi3){
+    console.log(sayi1+'en buyuk')
+}else{
+console.log(sayi3+'en buyuk')
+}
+
+var enbuyuk=sayi1 // diyelimki suan en buyuk sayi sayi1 
+
+if(enbuyuk<sayi2){
+  enbuyuk=sayi2
+}else{
+    enbuyuk=sayi3
+}
+console.log(enbuyuk)
